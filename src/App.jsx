c5b30d009147d42a003efb167af76e7530f1cc03
adnanAbdulRahim <122385516+adnanAbdulRahim@users.noreply.github.com>
2023-08-29 17:04:00 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+//class is reservered in .jsx keyword so we use className.
+//Can use Fragments (<></>) to return multiple components from a component.
+
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
+
+export default function App() {
+  //localStorage.getItem() retrieves item from local storage so data isn't lost.
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  //useEffect event listener paired with localStorage.setItem() allows us to store information in local storage anytime [todo] array has a change.
+  //make SURE to use hook at the top of the file to not run into errors eg:useEffect(), useState().
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  )
+}
